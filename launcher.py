3eb4ab4460de9b5f9f9c79f0f699584b5f551bbe
Remi TAUVEL <tauvel.remi@gmail.com>
2019-11-18 23:31:34 +0100

    wall collisions

--- launcher.py
+++ launcher.py
@@ -7,17 +7,28 @@ while exit==False:
     print(maze)
     dir = input("quelle direction?")
     if dir == "z":
-        maze.MG.y-=1
-        print("mouvement vers le haut")
+        if maze.map[maze.MG.x][maze.MG.y-1].genre!="M":
+            maze.MG.y-=1
+            print("mouvement vers le haut")
+        else:
+            print("Ouch! There's a Wall!")
     elif dir=="q":
-        maze.MG.x-=1
-        print("mouvement vers la gauche")
+        if maze.map[maze.MG.x-1][maze.MG.y].genre!="M":
+            maze.MG.x-=1
+            print("mouvement vers la gauche")
+        else:
+            print("Ouch! There's a Wall!")
     elif dir == "s":
-        maze.MG.y+=1
-        print("mouvement vers le bas")
+        if maze.map[maze.MG.x][maze.MG.y+1].genre!="M":
+            maze.MG.y+=1
+            print("mouvement vers le bas")
+        else:
+            print("Ouch! There's a Wall")
     elif dir == "d":
-        maze.MG.x+=1
-        print("mouvement vers la droite")
+        if maze.map[maze.MG.x+1][maze.MG.y].genre!="M":
+            maze.MG.x+=1
+            print("mouvement vers la droite")
+        else:
+            print("Ouch! There's a Wall")
     else:
         exit = True
-        
