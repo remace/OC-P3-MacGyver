9fac6d41ca30b68cde6a9c290f917dc9fdb559b0
Remi TAUVEL <tauvel.remi@gmail.com>
2019-11-27 21:26:30 +0100

    pep8

--- src/characters.py
+++ src/characters.py
@@ -7,21 +7,22 @@ class Character:
 class Hero(Character):
     """class defining a hero, mainly MacGyver in this game"""
     def __init__(self, x, y):
-        "constructor for a Hero"
-        Character.__init__(self, x ,y )
-        self.name="Mac Gyver"
+        """constructor for a Hero"""
+        Character.__init__(self, x, y)
+        self.name = "Mac Gyver"
         self.inventory = {}
     
     def gather_item(self, i):
         """gather an Item on the floor at the position of the hero.
         i is an Item"""
-        self.inventory[i.name] = [i.x,i.y]
+        self.inventory[i.name] = [i.x, i.y]
         
 
 class Villain(Character):
     def __init__(self, name, x, y, di1, di2, di3):
+        """constructor for a villain"""
         Character.__init__(self, x, y)
-        self.name = "Garde"
+        self.name = name
         self.death_items = []
         self.death_items.append(di1)
         self.death_items.append(di2)
