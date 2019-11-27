a31d25cf2777ccd51ee77c2ae10cef8f473c77cd
remace <tauvel.remi@gmail.com>
2019-11-27 14:30:58 +0100

    translating some const chars and comments in english

--- mapGeneration/creerMap.py
+++ mapGeneration/creerMap.py
@@ -1,4 +1,4 @@
-""" génération d'une map pour faire des tests. """
+""" map generation for tests """
 
 
 def createMaze(width,height):
@@ -16,12 +16,12 @@ def createMaze(width,height):
         
         f.write('\n\n')#on passe aux personnages
         f.write('Mac Gyver\t2\t8\n')
-        f.write('Garde\t6\t8\taiguille\ttuyau\tseringue\t')
+        f.write('Garde\t6\t8\tneedle\tpipe\tsyringe\t')
 
         f.write("\n\n")#on passe aux objets
-        f.write("aiguille\t3\t8\n")
-        f.write("tuyau\t4\t8\n")
-        f.write("seringue\t5\t8\n")
+        f.write("needle\t3\t8\n")
+        f.write("pipe\t4\t8\n")
+        f.write("syringe\t5\t8\n")
 
 if __name__=="__main__":
     createMaze(10,10)
